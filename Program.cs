24ff9eeb114311195e237c1660cf5904d5aed7ce
Dimitar Dzhambazov <129491664+dzambazov1986@users.noreply.github.com>
2024-01-24 14:10:01 +0200

    P1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string typeFigure = Console.ReadLine();
+if (typeFigure == "square")
+{ double side = double.Parse(Console.ReadLine());
+    double area = side * side;
+    Console.WriteLine($"{area:F2}");
+}
+else if (typeFigure == "rectangle")
+{
+    double width = double.Parse(Console.ReadLine());    
+    double lenght = double.Parse(Console.ReadLine());
+
+    double area = width * lenght;
+    Console.WriteLine($"{area:F2}");
+}
+else if (typeFigure == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());   
+    double area = radius * radius * Math.PI;
+    Console.WriteLine($"{area:F2}");
+}
