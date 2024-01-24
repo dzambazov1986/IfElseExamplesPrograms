6fa9d48d27ddde3933b04b643cbdd7299b51cfab
Dimitar Dzhambazov <129491664+dzambazov1986@users.noreply.github.com>
2024-01-24 14:10:22 +0200

    P2

--- Program.cs
+++ Program.cs
@@ -1,20 +1,3 @@
-﻿string typeFigure = Console.ReadLine();
-if (typeFigure == "square")
-{ double side = double.Parse(Console.ReadLine());
-    double area = side * side;
-    Console.WriteLine($"{area:F2}");
-}
-else if (typeFigure == "rectangle")
-{
-    double width = double.Parse(Console.ReadLine());    
-    double lenght = double.Parse(Console.ReadLine());
-
-    double area = width * lenght;
-    Console.WriteLine($"{area:F2}");
-}
-else if (typeFigure == "circle")
-{
-    double radius = double.Parse(Console.ReadLine());   
-    double area = radius * radius * Math.PI;
-    Console.WriteLine($"{area:F2}");
-}
+﻿int temperature = int.Parse(Console.ReadLine());
+if (temperature < 0)
+    Console.WriteLine($"{"Freezing weather!"}");
