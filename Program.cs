fc183c0fefe8f753e967eb55934804816f6dca9b
Dimitar Dzhambazov <129491664+dzambazov1986@users.noreply.github.com>
2024-01-24 14:11:40 +0200

    P6

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
-﻿using System.ComponentModel.Design;
+﻿string password = (Console.ReadLine());   
 
-int number1 = int.Parse(Console.ReadLine());
-int number2 =  int.Parse(Console.ReadLine());
-
-
-if (number1 > number2)
+if  (password == "s3cr3t!")
 {
-    Console.WriteLine($"Greater number:{number1}");
+    Console.WriteLine("Welcome");
 }
- else if (number1 < number2)
-    {
-    Console.WriteLine($"Greater number:{number2}");
-     }
-     
+else
+{
+    Console.WriteLine("Wrong password!");
+}
